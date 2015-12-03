37d5b068cf68e0e75bc035239b460cab4442b0e5
4nn4bel <annabeltengingsing@gmail.com>
2015-12-03 16:36:10 +0800

    added we chat and its network transport permission

---
+++ WeChatActivity.h
@@ -0,0 +1,13 @@
+//
+//  WeChatActivity.h
+//  shareprototype
+//
+//  Created by Bel Work Projects on 3/12/15.
+//  Copyright © 2015 Bel Work Projects. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WeChatActivity : UIActivity
+
+@end
---
+++ WeChatActivity.m
@@ -0,0 +1,13 @@
+//
+//  WeChatActivity.m
+//  shareprototype
+//
+//  Created by Bel Work Projects on 3/12/15.
+//  Copyright © 2015 Bel Work Projects. All rights reserved.
+//
+
+#import "WeChatActivity.h"
+
+@implementation WeChatActivity
+
+@end
