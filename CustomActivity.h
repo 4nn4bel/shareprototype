caf00e1d20cc886e7189a944a343ce0718f1fd29
4nn4bel <annabeltengingsing@gmail.com>
2015-12-03 14:10:54 +0800

    added custom activity

---
+++ CustomActivity.h
@@ -0,0 +1,14 @@
+//
+//  CustomActivity.h
+//  shareprototype
+//
+//  Created by Bel Work Projects on 3/12/15.
+//  Copyright © 2015 Bel Work Projects. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomActivity : UIActivity
+
+
+@end
---
+++ CustomActivity.m
@@ -0,0 +1,13 @@
+//
+//  CustomActivity.m
+//  shareprototype
+//
+//  Created by Bel Work Projects on 3/12/15.
+//  Copyright © 2015 Bel Work Projects. All rights reserved.
+//
+
+#import "CustomActivity.h"
+
+@implementation CustomActivity
+
+@end
